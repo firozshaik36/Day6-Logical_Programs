a01c89786ab747cacfbd3cb9f77036829dffdf94
firozshaik36 <firozshaik0007@gmail.com>
2022-04-14 10:09:39 +0530

    Added: Day6 Logical Programs.

---
+++ ReverseNumber.java
@@ -0,0 +1,26 @@
+package com.bridgelabz;
+
+
+import java.util.Scanner;
+
+public class ReverseNumber {
+    public static void main(String []args){
+
+        Scanner sc = new Scanner(System.in);
+        int remainder , reversedNumber = 0;
+        System.out.println("Enter a Number to be Reversed:");
+        int numberToBeReverse = sc.nextInt();
+        sc.close();
+
+        while (numberToBeReverse > 0){
+
+            remainder = numberToBeReverse % 10;
+            reversedNumber = ((reversedNumber * 10) + remainder);
+            numberToBeReverse = numberToBeReverse / 10;
+
+        }
+
+        System.out.println("The Reversed Number is :" + reversedNumber);
+
+    }
+}
